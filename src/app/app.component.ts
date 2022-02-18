56daf5c816d0d270dbb38e3b43971357cb7b8510
JuandeBarco <34903133+JuandeBarco@users.noreply.github.com>
2022-02-18 17:00:25 -0600

    Ejemplos Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_pipes';
+
+  public nombre : string = "Juan de Dios";
+  public pi : number = Math.PI;
+  public porcentaje : number = 0.234;
+  public salario : number = 1234.5;
+  public fecha : Date = new Date();
+  public persona : any = {nombre : "Juan de Dios", apellido : "Barajas"};
+
+  public valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Llego la data'), 3500);
+  });
+
+  public activar : boolean = true;
+
+  public arrNumeros : number[] = [1,2,3,4,5,6,7,8,9,10];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContrasenaPipe } from './contrasena.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
